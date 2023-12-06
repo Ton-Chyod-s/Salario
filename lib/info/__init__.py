81b0bca3a83fd0fe77fc01cabf4da1f7a1d55c94
Klay <arqkdias@gmail.com>
2023-12-06 17:11:55 -0400

    att

---
+++ lib/info/__init__.py
@@ -0,0 +1,6 @@
+porcentagem = {
+	'Despesas': 50, #50% do salario no mês seram para as despesas
+	'Investimento': 30, #30% para os investimento tanto cursos como ativos
+	'Fundo Emergencial': 10, #10% para emergencial caso aconteça algum sinisto na vida
+	'Pode gastar': 10 #10% do salario vc podera gastar a toa, por que todo mundo tem direito a isso haha
+	}
