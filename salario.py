73901d0bf09ca6caeae0e9350807ce1046c66e1a
Klay <arqkdias@gmail.com>
2023-12-07 14:01:15 -0400

    doc: att

--- salario.py
+++ salario.py
@@ -2,13 +2,8 @@ from lib import calc,info
 import PySimpleGUI as sg
 dicionario = info.despesas(Despesas=50,Investimento=30,Fundo_Emergencial=10,Pode_gastar=10)
 
-salario = int(input(f'Digite seu salário: R$'))
-
-
-
 selected_theme = 'Reddit'
 sg.theme(selected_theme)
-
 
 layout_login = [
         [sg.Text('Salário:\t'),sg.Stretch(),sg.Input(size=(6,1),key='salario'),sg.Btn('ok',size=(3,1))],
@@ -19,11 +14,11 @@ window = sg.Window('Divisão', icon=' ',layout=layout_login, keep_on_top=True, f
 
 while True:
         event,values = window.read()
-        if event == sg.WIN_CLOSED or event == 'Sair': # if user closes window or clicks cancel
+        if event == sg.WIN_CLOSED or event == 'Sair': 
             break
         
         if event == 'ok':
-            calc.calculo(dicionario,salario)
+            calc.calculo(dicionario,int(values['salario']))
 
 window.close()
 
