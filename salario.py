484bea23a869354ce37758dddeb9677e00c602cb
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-03-22 16:18:58 -0400

    doc: att

--- salario.py
+++ salario.py
@@ -8,6 +8,7 @@ sg.theme(selected_theme)
 layout_login = [
         [sg.Text('Salário:\t'),sg.Stretch(),sg.Input(size=(6,1),key='salario'),sg.Btn('ok',size=(3,1))],
         [sg.Output(size=(25,6), key = '_output_')],
+        [sg.Button('%',size=(5,1))],
         ]
 
 window = sg.Window('Divisão', icon=' ',layout=layout_login, keep_on_top=True, finalize = True)
@@ -20,6 +21,23 @@ while True:
         if event == 'ok':
             window.FindElement('_output_').Update('')
             calc.calculo(dicionario,int(values['salario']))
+
+        if event == '%':
+            layout_login = [
+                    [sg.Text('Despesas:\t'),sg.Stretch(),sg.Input(size=(6,1),key='desp')],
+                    [sg.Text('Investimento:\t'),sg.Stretch(),sg.Input(size=(6,1),key='inv')],
+                    [sg.Text('Fundo_Emergencial:\t'),sg.Stretch(),sg.Input(size=(6,1),key='fe')],
+                    [sg.Text('Pode_gastar:\t'),sg.Stretch(),sg.Input(size=(6,1),key='pg')],
+                    [sg.Button('confimar',size=(5,1))],
+                    ]
+
+            window = sg.Window('Divisão', icon=' ',layout=layout_login, keep_on_top=True, finalize = True)
+
+        while True:
+             event,values = window.read()
+             
+             if event == 'confimar':
+                info.mudar_conteudo_valor(Despesas=50,Investimento=30,Fundo_Emergencial=10,Pode_gastar=10)
 
 window.close()
 
