ab5c8f7867275b9baac823d47e133f7a44600af2
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-03-25 08:01:30 -0400

    doc: att

--- salario.py
+++ salario.py
@@ -5,13 +5,13 @@ dicionario = info.despesas(Despesas=60,Investimento=30,Fundo_Emergencial=5,Pode_
 selected_theme = 'Reddit'
 sg.theme(selected_theme)
 
-layout_login = [
+layout = [
         [sg.Text('Salário:\t'),sg.Stretch(),sg.Input(size=(6,1),key='salario'),sg.Btn('ok',size=(3,1))],
         [sg.Output(size=(25,6), key = '_output_')],
         [sg.Button('%',size=(5,1))],
         ]
 
-window = sg.Window('Divisão', icon=' ',layout=layout_login, keep_on_top=True, finalize = True)
+window = sg.Window('Divisão', icon=' ',layout=layout, keep_on_top=True, finalize = True)
 
 while True:
         event,values = window.read()
@@ -23,7 +23,7 @@ while True:
             calc.calculo(dicionario,int(values['salario']))
 
         if event == '%':
-            layout_login = [
+            layout_porc = [
                     [sg.Text('Despesas:\t'),sg.Stretch(),sg.Input(size=(6,1),key='desp')],
                     [sg.Text('Investimento:\t'),sg.Stretch(),sg.Input(size=(6,1),key='inv')],
                     [sg.Text('Fundo_Emergencial:\t'),sg.Stretch(),sg.Input(size=(6,1),key='fe')],
@@ -31,18 +31,17 @@ while True:
                     [sg.Button('confimar',size=(8,1))],
                     ]
 
-            window = sg.Window('Divisão', icon=' ',layout=layout_login, keep_on_top=True, finalize = True)
+            window_porc = sg.Window('porcentagem', icon=' ',layout=layout_porc, keep_on_top=True, finalize = True)
 
             while True:
-                event,values = window.read()
+                event,values = window_porc.read()
                 if event == sg.WIN_CLOSED or event == 'Sair': 
                     break
 
                 if event == 'confimar':
-                    info.despesas(Despesas=values['desp'],Investimento=values['inv'],Fundo_Emergencial=values['fe'],Pode_gastar=values['pg'])
+                    info.despesas(Despesas=int(values['desp']),Investimento=int(values['inv']),Fundo_Emergencial=int(values['fe']),Pode_gastar=int(values['pg']))
                     
-                window.close()
-
+            window_porc.close()
 window.close()
 
 
