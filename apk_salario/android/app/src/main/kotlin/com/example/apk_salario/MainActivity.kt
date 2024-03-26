47141b8a0ecfda1fc8814cdf20206b134525306a
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-03-26 13:48:30 -0400

    doc: att

--- apk_salario/android/app/src/main/kotlin/com/example/apk_salario/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.apk_salario
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ aplicativo/android/app/src/main/kotlin/com/example/aplicativo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.aplicativo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
